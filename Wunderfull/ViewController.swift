5115d2c43ec459eddc3641cec4ed3f8663757b92
Peter Pult <peter@pult.cc>
2016-11-25 22:48:37 +0100

    Initial Commit

---
+++ Wunderfull/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wunderfull
+//
+//  Created by Peter Pult on 25/11/2016.
+//  Copyright © 2016 Peter Pult. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
